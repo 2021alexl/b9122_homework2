b269ae9024cc358f4e3a4499221b542ecf975ab2
Alex Lan <90366760+2021alexl@users.noreply.github.com>
2023-10-07 19:30:36 -0400

    Add files via upload

--- B9122_HW_2_sol_Ke_Lan.py
+++ B9122_HW_2_sol_Ke_Lan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[9]:
 
 
 import pandas as pd
@@ -10,13 +10,13 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# In[2]:
+# In[1]:
 
 
 url = "https://press.un.org/en"
 
 
-# In[3]:
+# In[2]:
 
 
 def is_press_release(soup):
@@ -27,7 +27,7 @@ def is_press_release(soup):
         return False
 
 
-# In[4]:
+# In[3]:
 
 
 def is_plenary_session(soup):
@@ -38,7 +38,7 @@ def is_plenary_session(soup):
         return False
 
 
-# In[5]:
+# In[4]:
 
 
 def contains_crisis(soup):
@@ -48,7 +48,7 @@ def contains_crisis(soup):
         return False
 
 
-# In[36]:
+# In[5]:
 
 
 def add_link(link, visited, q):
@@ -63,7 +63,7 @@ def add_link(link, visited, q):
     return None
 
 
-# In[38]:
+# In[6]:
 
 
 def find_press_release(url, visited, q):
@@ -88,7 +88,7 @@ def find_press_release(url, visited, q):
     return press_release, links
 
 
-# In[37]:
+# In[7]:
 
 
 def get_press_releases(seed_url, q, print = False):
@@ -116,17 +116,20 @@ def get_press_releases(seed_url, q, print = False):
 
 # # Q1
 
-# In[24]:
+# In[10]:
 
 
 urls_1 = get_press_releases('https://press.un.org/en' , 'Q1')
 
 
-# In[26]:
+# In[11]:
 
 
+strs = []
 for u in urls_1:
     print(u)
+    strs += [u]
+    
 
 
 # # Q2
@@ -142,4 +145,72 @@ urls_2 = get_press_releases('https://www.europarl.europa.eu/news/en/press-room',
 
 for u in urls_2:
     print(u)
+
+
+# # Q2 After ED 
+
+# In[14]:
+
+
+url = 'https://www.europarl.europa.eu/news/en/press-room/page/0'
+
+
+# In[15]:
+
+
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+
+# In[21]:
+
+
+viable_link = []
+for i in range(10):
+    url = 'https://www.europarl.europa.eu/news/en/press-room/page/'
+    url += str(i)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    if len(viable_link) == 10:
+        break
+    for item in soup.find_all('a', href=True):
+        if 'https://www.europarl.europa.eu/news/en/press-room/20' in item['href']:
+            url = item['href']
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+            if is_plenary_session(soup) and contains_crisis(soup):
+                print(url)
+                viable_link += [url]
+
+
+# In[22]:
+
+
+urls_1[0]
+
+
+# In[25]:
+
+
+for i in range(1,11):
+    txts = '1_'
+    txts += str(i)
+    txts += '.txt'
+    with open(txts, "w") as file:
+        file.write(urls_1[i-1])
+        
+# NEXT TIME THINK THROUGH THEN ASSIGN YOUR HOMEWORK
+
+
+# In[26]:
+
+
+for i in range(1,11):
+    txts = '2_'
+    txts += str(i)
+    txts += '.txt'
+    with open(txts, "w") as file:
+        file.write(viable_link[i-1])
+        
+# ALRIGHT?
 
